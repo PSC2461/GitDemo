fca893bc37f62e04e7bc6546339d28bb34a2d17d
PSC2461 <meetpunjaji@gmail.com>
2021-02-05 21:11:07 -0600

    This changes done by Punjaji for testing

--- src/test.java
+++ src/test.java
@@ -3,10 +3,11 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int b=8;
 		int a=5;
 		System.out.println(a);
-
+		System.out.println(b);
+		System.out.println(a+b);
 	}
 
 }
